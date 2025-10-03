f904d02a1d84b5f848661645eed0d008d573b875
Josh Dutton <830349+jbdutton@users.noreply.github.com>
2025-10-03 11:57:05 -0400

    Accept target and metadata params in createZodDto

--- packages/nestjs-zod/src/dto.ts
+++ packages/nestjs-zod/src/dto.ts
@@ -1,10 +1,23 @@
 import { UnknownSchema } from './types'
 import type * as z3 from 'zod/v3';
-import { toJSONSchema, $ZodType, JSONSchema } from "zod/v4/core";
+import * as z from 'zod/v4';
+import { toJSONSchema, $ZodType, JSONSchema, $ZodRegistry } from "zod/v4/core";
 import { assert } from './assert';
 import { DEFS_KEY, EMPTY_TYPE_KEY, HAS_CONST_KEY, HAS_NULL_KEY, PARENT_ADDITIONAL_PROPERTIES_KEY, PARENT_HAS_REFS_KEY, PARENT_ID_KEY, PREFIX, UNWRAP_ROOT_KEY } from './const';
 import { walkJsonSchema } from './utils';
 import { zodV3ToOpenAPI } from './zodV3ToOpenApi';
+
+export interface SchemaParams {
+    /** A registry used to look up metadata for each schema. Any schema with an `id` property will be extracted as a $def.
+     *  @default globalRegistry */
+    metadata?: $ZodRegistry<Record<string, any>>;
+    /** The JSON Schema version to target.
+     * - `"draft-2020-12"` — Default. JSON Schema Draft 2020-12
+     * - `"draft-7"` — JSON Schema Draft 7
+     * - `"draft-4"` — JSON Schema Draft 4
+     * - `"openapi-3.0"` — OpenAPI 3.0 Schema Object */
+    target?: "draft-4" | "draft-7" | "draft-2020-12" | "openapi-3.0";
+}
 
 export interface ZodDto<
   TSchema extends UnknownSchema
@@ -19,7 +32,7 @@ export interface ZodDto<
 
 export function createZodDto<
   TSchema extends UnknownSchema|z3.ZodTypeAny|($ZodType & { parse: (input: unknown) => unknown })
->(schema: TSchema) {
+>(schema: TSchema, registry?: $ZodRegistry<{description: string}>, params?: SchemaParams) {
   class AugmentedZodDto {
     public static readonly isZodDto = true
     public static readonly schema = schema
@@ -42,7 +55,7 @@ export function createZodDto<
         }
 
         public static _OPENAPI_METADATA_FACTORY() {
-          return openApiMetadataFactory({ schema: this.schema, io: "output" });
+          return openApiMetadataFactory({ schema: this.schema, params: params, io: "output" });
         }
       }
 
@@ -52,7 +65,7 @@ export function createZodDto<
     }
 
     public static _OPENAPI_METADATA_FACTORY() {
-      return openApiMetadataFactory({ schema: this.schema, io: "input" });
+      return openApiMetadataFactory({ schema: this.schema, params: params, io: "input" });
     }
   }
 
@@ -61,9 +74,11 @@ export function createZodDto<
 
 function openApiMetadataFactory({ 
   schema, 
+  params,
   io,
 }: { 
   schema: UnknownSchema | z3.ZodTypeAny | ($ZodType & { parse: (input: unknown) => unknown; }), 
+  params?: SchemaParams,
   io: 'input' | 'output',
 }) {
   if (!('_zod' in schema) && '_def' in schema && io === 'output') {
@@ -74,7 +89,7 @@ function openApiMetadataFactory({
     return {};
   }
 
-  const { $defs, $schema, ...generatedJsonSchema } = generateJsonSchema(schema, io);
+  const { $defs, $schema, ...generatedJsonSchema } = generateJsonSchema(schema, io, params);
 
   /**
    * nestjs expects us to return a record of properties
@@ -102,7 +117,7 @@ function openApiMetadataFactory({
     ...generatedJsonSchema,
     $defs,
   };
-  
+
   const { hasRefs, hasNull, hasConst } = getSchemaMetadata(jsonSchema);
 
   let properties: Record<string, unknown> = {};
@@ -184,15 +199,25 @@ function openApiMetadataFactory({
   return properties;
 }
 
-function generateJsonSchema(schema: z3.ZodTypeAny | ($ZodType & { parse: (input: unknown) => unknown; }), io: 'input' | 'output') {
+function generateJsonSchema(
+  schema: z3.ZodTypeAny | ($ZodType & { parse: (input: unknown) => unknown; }),
+  io: 'input' | 'output',
+  params?: SchemaParams) {
   const generatedJsonSchema = '_zod' in schema ? toJSONSchema(schema, {
     io,
+    target: params?.target ?? 'openapi-3.0',
+    metadata: params?.metadata ?? z.globalRegistry,
     override: ({ jsonSchema }) => {
         if (io === 'output' && 'id' in jsonSchema) {
             jsonSchema.id = `${jsonSchema.id}_Output`;
         }
     } 
   }) : zodV3ToOpenAPI(schema)
+
+  if ('_zod' in schema) {
+    const sch = z.globalRegistry.get(schema);
+    console.log(`sch? ${sch} | ${sch?.description}`);
+  }
 
   const $defs = ('$defs' in generatedJsonSchema && generatedJsonSchema.$defs) ? generatedJsonSchema.$defs : undefined;
 
